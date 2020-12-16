2a1802444cd0e0fe76cc54097ad33f3ba7ea658b
cleitonts <cleitonsorrilha@gmail.com>
2020-12-16 12:41:51 -0300

    atualizando ambiente de producao

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-phpinfo();
-
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
